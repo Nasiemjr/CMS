7c6bd9fb6e0ff9e2d157a7d41ea4cb6dca2521ba
Nasiemjr <Nasiem1610@gmail.com>
2024-08-06 23:03:00 +0200

    Initial commit.

---
+++ CMS/Models/ContactDBContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace CMS.Models
+{
+    public class ContactDBContext : DbContext
+    {
+        public DbSet<Contact> Contact { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseInMemoryDatabase("CMSDB");
+        }
+    }
+}
